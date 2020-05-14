5ed4331b3ad5b5583ef17056f07f70d28c66b723
Johan Brinch <johan@pleo.io>
2020-05-14 14:56:14 +0200

    Initial import of silly script

---
+++ py/overlay.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+# setup: pip3 install scikit-image
+from skimage import io, draw
+
+# read hospitalizations per day from datafile
+points = list(map(int, open('hospitalized.txt').readlines()))
+
+# read the forecast image
+img = io.imread('forecast.jpg')
+
+# short-hand to draw a line from (x0, y0) to (x1, y1)
+def add_line(img, x0, y0, x1, y1):
+  cc, rr, val = draw.line_aa(int(y0), int(x0), int(y1), int(x1))
+  img[cc, rr] = (255, 0, 0)
+
+# coordinate for (0, 0) on the plot
+start_x = 89
+start_y = 477
+
+# the spacing between x-axis points (days)
+x_point_width = 4.1
+
+# the spacing between y-axis points (patients)
+y_point_width = 0.18
+
+# we have 523 pixels between the 3 plots
+# we draw our plot on top of each
+for offset_y in [0, 523, 523*2]:
+  prev_x = start_x
+  prev_y = start_y + offset_y
+  for x, p in enumerate(points):
+    new_x = start_x + x * x_point_width
+    new_y = start_y - p * y_point_width + offset_y
+    add_line(img, prev_x, prev_y, new_x, new_y)
+    prev_x = new_x
+    prev_y = new_y
+
+# save the result
+io.imsave('result.jpg', img)
