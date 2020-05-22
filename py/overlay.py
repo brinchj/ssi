22219ea8216c30dea562e31b129dac3779a3df09
Johan Brinch <johan@pleo.io>
2020-05-22 17:01:30 +0200

    Fit to existing graph (rotation? stretching?)

--- py/overlay.py
+++ py/overlay.py
@@ -14,12 +14,15 @@ def add_line(img, x0, y0, x1, y1):
   cc, rr, val = draw.line_aa(int(y0), int(x0), int(y1), int(x1))
   img[cc, rr] = (255, 0, 0)
 
+fix_x = 0.95
+fix_y = 1.05
+
 # coordinate for (0, 0) on the plot
-start_x = 80
+start_x = 84
 start_y = 405
 
 # the spacing between x-axis points (days)
-x_point_width = 3.77
+x_point_width = 3.76
 
 # the spacing between y-axis points (patients)
 y_point_width = 0.165
@@ -31,8 +34,8 @@ for offset_y in [0, 481, 481*2]:
   prev_y = start_y + offset_y
   for x, p in enumerate(points):
     new_x = start_x + x * x_point_width
-    new_y = start_y - p * y_point_width + offset_y
-    add_line(img, prev_x, prev_y, new_x, new_y)
+    new_y = start_y - fix_y * p * y_point_width + offset_y
+    add_line(img, prev_x * fix_x, prev_y, new_x * fix_x, new_y)
     prev_x = new_x
     prev_y = new_y
 
