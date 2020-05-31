3240d76445fb09dcdba2a4a94fc963bd212bee14
Johan Brinch <johan@pleo.io>
2020-05-31 12:22:43 +0200

    Validation: x-axis was slightly wrong

--- py/overlay.py
+++ py/overlay.py
@@ -1,13 +1,15 @@
 import lib
 
-data = lib.Prediction('reports/20200506/22.png', x_size=110, y_size=2000)
+days = 112
+
+data = lib.Prediction('reports/20200506/22.png', x_size=days, y_size=2000)
 img1 = [data.process(1010, 1064), data.process(1010, 1702), data.process(1010, 2334)]
 
 p = (1034, 2490)
 img2 = [
-    lib.Prediction('reports/20200520/13.png', x_size=110, y_size=2000).process(*p),
-    lib.Prediction('reports/20200520/14.png', x_size=110, y_size=2000).process(*p),
-    lib.Prediction('reports/20200520/15.png', x_size=110, y_size=2000).process(*p),
+    lib.Prediction('reports/20200520/13.png', x_size=days, y_size=2000).process(*p),
+    lib.Prediction('reports/20200520/14.png', x_size=days, y_size=2000).process(*p),
+    lib.Prediction('reports/20200520/15.png', x_size=days, y_size=2000).process(*p),
 ]
 
 lib.save(lib.merge([img1, img2]), 'result.png')
