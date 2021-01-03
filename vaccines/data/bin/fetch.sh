4d500c8179a1612f796118647f6b6202ff7efa71
Johan Brinch <johan@pleo.io>
2021-01-03 17:10:49 +0100

    Add fetch script

---
+++ vaccines/data/bin/fetch.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -euo pipefail
+
+curl $(curl https://covid19.ssi.dk/overvagningsdata/vaccinationstilslutning|rg 'https://files.ssi.dk/covid19/vaccinationstilslutning/vaccinationstilslutning-[0-9a-z-]+' -o|head -n1) > vaccinationstilslutning.pdf
+
+curl $(curl https://covid19.ssi.dk/overvagningsdata/download-fil-med-overvaagningdata|rg 'https://files.ssi.dk/covid19/overvagning/data/data-epidemiologiske-rapport-[0-9a-z-]+' -o|head -n1) > data-epidemiologiske-rapport.zip
