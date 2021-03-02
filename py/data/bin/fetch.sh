b04d872394e6873015c82e4d64d593283545e442
Johan Brinch <johan@pleo.io>
2021-03-02 14:27:45 +0100

    Update

--- py/data/bin/fetch.sh
+++ py/data/bin/fetch.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-curl $(curl https://covid19.ssi.dk/overvagningsdata/download-fil-med-overvaagningdata|rg 'https://files.ssi.dk/covid19/overvagning/data/data-epidemiologiske-rapport-[0-9a-z-]+' -o|head -n1) > data-epidemiologiske-rapport.zip
+curl $(curl https://covid19.ssi.dk/overvagningsdata/download-fil-med-overvaagningdata|rg 'https://files.ssi.dk/covid19/overvagning/data/data-[0-9a-z-]+' -o|head -n1) > data-epidemiologiske-rapport.zip
 
 unzip data-epidemiologiske-rapport.zip
 
